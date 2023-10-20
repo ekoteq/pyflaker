75cd3977324a29c794a6dc8b7c12dc0034432544
drix <106333760+ekoteq@users.noreply.github.com>
2023-10-19 22:01:16 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,394 +1,250 @@
 from __future__ import annotations
 
-from asyncio import Lock, Event, Semaphore, sleep
 from datetime import datetime
-from typing import TYPE_CHECKING
+from time import sleep
+from threading import (
+    Event,
+    Lock,
+)
+from typing import (
+    TYPE_CHECKING,
+    Generic,
+    TypeVar,
+)
 
 if TYPE_CHECKING:
-    from typing import Any, Coroutine, Final, Literal, Optional, Tuple, TypeAlias, Union
-
-    DateLike: TypeAlias = Union[
-        str,
-        int,
-        float,
-        datetime,
-    ]
-
-__all__: Tuple[str, ...] = (
-    'snowflake_to_datetime',
-    'generate_snowflake',
-    'SnowflakeGenerator',
-)
-
-EKO_EPOCH: Final[datetime] = datetime(
-    year = 2019,
-    month = 4,
-    day = 15,
-    hour = 4,
-    minute = 12,
-)
-
-def snowflake_to_datetime(
-    snowflake: int,
-    epoch: datetime,
-) -> datetime:
-    """
-    Convert a snowflake ID to a datetime object.
-
-    Parameters
-    ----------
-
-        snowflake: :class:`int`
-            The snowflake ID to convert.
-
-        epoch: datetime
-            The epoch datetime value used during snowflake ID creation.
-
-    Returns
-    -------
-        :class:`datetime`
-            The datetime object from the snowflake ID.
-
-    """
-
-    return datetime.fromtimestamp(((snowflake >> 22) + int(epoch.timestamp() * 1000)) / 1000)
-
-async def generate_snowflake(
-    epoch: datetime = EKO_EPOCH,
-    worker_id: Optional[int] = 0,
-    process_id: Optional[int] = 0,
-    *,
-    last_timestamp: Optional[datetime] = datetime.now(),
-    sequence: Optional[int] = 0,
-) -> Tuple[int, datetime, int]:
-    """
-    Generates a snowflake ID based on the provided parameters.
-
-    Parameters
-    ----------
-
-        epoch: :class:`int`
-            The epoch timestamp to use for the snowflake.
-
-        worker_id: Optional[:class:`int`]
-            The worker ID to use for the snowflake.
-
-        process_id: Optional[:class:`int`]
-            The process ID to use for the snowflake.
-
-        last_timestamp: Optional[:class:`int`]
-            The last timestamp used for the snowflake. Defaults
-            to `0`.
-
-        sequence: Optional[:class:`int`]
-            The sequence to use for the snowflake. Defaults to
-            `0`.
-
-    Returns
-    -------
-        :class:`int`
-            The generated snowflake ID.
-
-    """
-    now  = datetime.utcnow()
-
-    while True:
-        now  = datetime.utcnow()
-
-        if last_timestamp > now: # type: ignore[operator]
-            # moving backwards in time
-            await sleep((last_timestamp - now).total_seconds()) # type: ignore[operator]
-            continue
-
-        if last_timestamp == now:
-            sequence = (sequence + 1) & 4095  # type: ignore[operator]
-
-            if sequence == 0:
-                await sleep(1/1000)
-                continue
-        else:
-            sequence = 0
-
-        break
-
-    return ((
-        (int((now.timestamp() * 1000) - (epoch.timestamp() * 1000)) << 22) |
-        (worker_id << 17) |  # type: ignore[operator]
-        (process_id << 12) |  # type: ignore[operator]
-        sequence
-    ), now, sequence)
-
-class SnowflakeGenerator:
-    if TYPE_CHECKING:
-        __lock: Lock
-        _frozen: bool
-        _locked: Event
-        _unlocked: Event
-        _counter: Semaphore
-        _sequence: int
-        _last_timestamp: datetime
-        epoch: datetime
-        worker_id: int
-        process_id: int
-
-    __slots__: Tuple[str, ...] = (
-        '__lock',
-        '_frozen',
-        '_locked',
-        '_unlocked',
-        '_counter',
-        '_sequence',
-        '_last_timestamp',
-        'name',
-        'epoch',
-        'worker_id',
-        'process_id',
+    from typing import (
+        Any,
+        Optional,
+        Tuple,
+        Type,
     )
+    
+__all__: Tuple[str, ...] = (
+    'Snowflakes',
+)
 
+_SnowflakeType = TypeVar(
+    '_SnowflakeType',
+    bound = int,
+)
+
+timestamp_bits: int = 2**64
+process_id_bits: int = 2**5
+thread_id_bits: int = 2**5
+sequence_bits: int = 2**12
+snowflake_bits: int = 2**64
+
+class SnowflakeGenerator(Generic[_SnowflakeType]):
+    if TYPE_CHECKING:
+        _lock: Lock
+        _closed: Event
+        _epoch: datetime
+        _process_id: int
+        _thread_id: int
+        _step: int
+        _sequence: int
+        _last: datetime
+        
     def __init__(
         self: SnowflakeGenerator,
-        epoch: Optional[datetime] = EKO_EPOCH,
-        *,
-        worker_id: Optional[int] = 0,
-        process_id: Optional[int] = 0,
+        epoch: datetime,
+        process_id: int,
+        thread_id: int,
+        step: int,
+        sequence: int,
+        last: datetime,
     ) -> None:
-        self._frozen = False
-        self.__lock = Lock()
-        self._locked = Event()
-        self._unlocked = Event()
-        self._counter = Semaphore(0)
-        self._sequence = 0
-        self._last_timestamp = datetime.now()
-
-        self._setup(
-            epoch,
-            worker_id,
-            process_id,
-        )
-
-    def _set_epoch(
-        self,
-        epoch: Optional[datetime] = EKO_EPOCH,
-    ) -> None:
-        if self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._set_epoch()' can only be "
-                "called once per instance, and cannot be called "
-                f"after '{type(self).__name__}._setup()' has been called."
-            )
-
-        epoch_type = type(epoch)
-
+        self._lock = Lock()
+        self._closed = Event()
+        
         if not isinstance(epoch, datetime):
-            if isinstance(epoch, str):
-                try:
-                    epoch = datetime.fromisoformat(epoch)
-                except ValueError:
-                    epoch = epoch.replace(',', '')
+            raise TypeError(f'Invalid epoch: {epoch}')
 
-                if '.' in epoch:
-                    epoch = float(epoch)
+        self._epoch = epoch
 
-            if isinstance(epoch, int):
-                epoch = float(epoch / 1000)
+        if process_id >= process_id_bits:
+            raise ValueError(f'Invalid process id value (process_id value greater than 31): {process_id}')
 
-            if isinstance(epoch, float):
-                epoch = datetime.fromtimestamp(epoch)
+        self._process_id = process_id
 
-        if not isinstance(epoch, datetime):
-            raise TypeError(
-                f"'{type(self).__name__}._set_epoch()' 'epoch' "
-                "value must be an instance of 'int', 'float', or "
-                f"'datetime'; received an instance of {epoch_type.__name__!r}"
-            )
+        if thread_id >= thread_id_bits:
+            raise ValueError(f'Invalid thread id value (thread_id value greater than 31): {thread_id}')
 
-        self.epoch = epoch # type: ignore[arg-type]
+        self._thread_id = thread_id
+        
+        if step < 1:
+            raise ValueError(f'Invalid step value (step value less than 1): {step}')
+        
+        if step >= sequence_bits:
+            raise ValueError(f'Invalid step value (step value greater than 4,095): {step}')
 
-    def _set_worker_id(
-        self,
-        worker_id: Optional[int] = 0,
-    ) -> None:
-        if self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._set_worker_id()' can only be "
-                "called once per instance, and cannot be called "
-                f"after '{type(self).__name__}._setup()' has been called."
-            )
+        self._step = step
 
-        if not isinstance(worker_id, int):
-            raise TypeError(
-                f"'{type(self).__name__}._set_worker_id()' 'worker_id' "
-                "value must be an instance of 'int'; received an instance"
-                f" of {type(worker_id).__name__!r}"
-            )
+        if sequence >= sequence_bits:
+            raise ValueError(f'Invalid sequence (sequence value greater than 4,095): {sequence}')
 
-        self.worker_id = worker_id # type: ignore[arg-type]
+        self._sequence = sequence
 
-    def _set_process_id(
-        self,
-        process_id: Optional[int] = 0,
-    ) -> None:
-        if self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._set_process_id()' can only be "
-                "called once per instance, and cannot be called "
-                f"after '{type(self).__name__}._setup()' has been called."
-            )
+        if last is None:
+            last = datetime.now()
 
-        if not isinstance(process_id, int):
-            raise TypeError(
-                f"'{type(self).__name__}._set_process_id()' 'process_id' "
-                "value must be an instance of 'int'; received an instance"
-                f" of {type(process_id).__name__!r}"
-            )
+        if not isinstance(last, datetime):
+            raise TypeError(f'Invalid last (object is not instance of datetime): {last}')
 
-        self.process_id = process_id # type: ignore[arg-type]
-
-    def _setup(
+        self._last = last
+        
+    @property
+    def epoch(self: SnowflakeGenerator) -> datetime:
+        return self._epoch
+    
+    @property
+    def process_id(self: SnowflakeGenerator) -> int:
+        return self._process_id
+    
+    @property
+    def thread_id(self: SnowflakeGenerator) -> int:
+        return self._thread_id
+    
+    @property
+    def step(self: SnowflakeGenerator) -> int:
+        return self._step
+        
+    @property
+    def sequence(self: SnowflakeGenerator) -> int:
+        return self._sequence
+    
+    @sequence.setter
+    def sequence(
         self: SnowflakeGenerator,
-        epoch: Optional[datetime] = EKO_EPOCH,
-        worker_id: Optional[int] = 0,
-        process_id: Optional[int] = 0,
+        value: int,
     ) -> None:
-        if self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._setup() can only be called "
-                "once per instance."
-            )
+        if self.closed:
+            raise RuntimeError(f'Cannot modify sequence valaue (snowflake generator is closed)')
 
-        self._set_epoch(epoch)
-        self._set_worker_id(worker_id)
-        self._set_process_id(process_id)
-        self._frozen = True
+        if value >= sequence_bits:
+            raise ValueError(f'Invalid sequence (sequence value greater than 4,095): {value}')
+        
+        self._sequence = value
+    
+    @property
+    def last(self: SnowflakeGenerator) -> datetime:
+        return self._last
+    
+    @last.setter
+    def last(
+        self: SnowflakeGenerator,
+        value: datetime,
+    ) -> None:
+        if self.closed:
+            raise RuntimeError(f'Cannot modify last value (snowflake generator is closed)')
 
-    def __repr__(self: SnowflakeGenerator) -> str:
-        if not self.frozen:
-            return f"<{type(self).__name__} setup={False!r}>"
+        if not isinstance(value, datetime):
+            raise TypeError(f'Invalid last (object is not instance of datetime): {value}')
+        
+        self._last = value
 
-        return (
-            f"<SnowflakeGenerator epoch={self.epoch!r} worker_id"
-            f"={self.worker_id!r} process_id={self.process_id!r}"
-            f" counter={self._counter._value:,}>"
-        )
-
-    def __str__(self: SnowflakeGenerator) -> str:
-        return self.__repr__()
-
-    def __bool__(self: SnowflakeGenerator) -> bool:
-        return self.frozen and self.unlocked
-
-    async def generate(self: SnowflakeGenerator) -> int:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}.generate()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        await self._lock()
-
-        _id, self._last_timestamp, self._sequence = await generate_snowflake(
-            self.epoch,
-            self.worker_id,
-            self.process_id,
-            last_timestamp = self._last_timestamp,
-            sequence = self._sequence,
-        )
-
-        self._counter.release()
-        self._unlock()
-
-        return _id
-
-    def __call__(self: SnowflakeGenerator) -> Coroutine[Any, Any, int]:
-        if not self.frozen:
-            raise RuntimeError(
-                f"{type(self).__name__!r} cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        return self.generate()
-
-    def __aiter__(self: SnowflakeGenerator) -> SnowflakeGenerator:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}.__aiter__()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
+    def __iter__(self: SnowflakeGenerator) -> SnowflakeGenerator:
         return self
+    
+    def __next__(self: SnowflakeGenerator) -> int:
+        if self.closed:
+            raise StopIteration('Cannot get next snowflake (snowflake generator is closed)')
 
-    def __anext__(self: SnowflakeGenerator) -> Coroutine[Any, Any, int]:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}.__anext__()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
+        try:
+            return self.generate()
+        except (
+            RuntimeError,
+            ValueError,
+        ) as e:
+            raise StopIteration(f'Cannot get next snowflake ({e})')
+    
+    def generate(self: SnowflakeGenerator) -> int:
+        if self.closed:
+            raise RuntimeError(f'Cannot generate snowflake (snowflake generator is closed)')
 
-        return self.__call__()
+        self._lock.acquire()
 
-    @property
-    def frozen(self: SnowflakeGenerator) -> bool:
-        return self._frozen
-
-    @property
-    def locked(self: SnowflakeGenerator) -> bool:
-        return self.__lock.locked()
-
-    @property
-    def unlocked(self: SnowflakeGenerator) -> bool:
-        return not self.__lock.locked()
-
-    def to_datetime(
-        self,
-        snowflake: int,
-    ) -> datetime:
-        return snowflake_to_datetime(
-            snowflake,
-            self.epoch,
+        now: datetime
+        sequence: int
+        
+        while True:
+            now = datetime.now()
+        
+            if self.last > now:
+                sleep((self.last - now).total_seconds())
+                continue
+        
+            if self.last == now:
+                sequence = (self.sequence + self.step) & (sequence_bits - 1)
+            
+                if sequence == 0:
+                    sleep(self.step / 1000)
+                    continue
+            
+            else:
+                sequence = 0
+            
+            self.last = now
+            self.sequence = sequence
+        
+            break
+        
+        res = (
+            (int(now.timestamp() * 1000) - int(self.epoch.timestamp() * 1000)) << 22 |
+            self.process_id << 17 |
+            self.thread_id << 12 |
+            sequence
         )
+        
+        self._lock.release()
+        
+        if res >= snowflake_bits:
+            raise ValueError(f'Invalid snowflake (snowflake value greater than 2^64): {res}')
+        
+        return res
+    
+    def close(self: SnowflakeGenerator) -> None:
+        self._closed.set()
+        
+    @property
+    def closed(self: SnowflakeGenerator) -> bool:
+        return self._closed.is_set()
 
-    async def _lock(self: SnowflakeGenerator) -> None:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._lock()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        if self.locked:
-            raise RuntimeError("Cannot lock while already locked")
-
-        await self.__lock.acquire()
-        self._unlocked.clear()
-        self._locked.set()
-
-    def wait_locked(self: SnowflakeGenerator) -> Coroutine[Any, Any, Literal[True]]:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}.wait_locked()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        return self._locked.wait()
-
-    def _unlock(self: SnowflakeGenerator) -> None:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}._unlock()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        if self.unlocked:
-            raise RuntimeError("Cannot unlock while not locked")
-
-        self.__lock.release()
-        self._locked.clear()
-        self._unlocked.set()
-
-    def wait_unlocked(self: SnowflakeGenerator) -> Coroutine[Any, Any, Literal[True]]:
-        if not self.frozen:
-            raise RuntimeError(
-                f"'{type(self).__name__}.wait_unlocked()' cannot be called "
-                "before '{type(self).__name__}._setup()' has been called"
-            )
-
-        return self._unlocked.wait()
+if __name__ == "__main__":
+    epoch = datetime(
+        2019,
+        4,
+        15,
+        4,
+        12,
+    )
+    process_id = 31
+    thread_id = 31
+    step = 1
+    sequence = 0
+    last = datetime.now()
+    
+    snowflake_generator = SnowflakeGenerator(
+        epoch = epoch,
+        process_id = process_id,
+        thread_id = thread_id,
+        step = step,
+        sequence = sequence,
+        last = last,
+    )
+    
+    print(f'epoch: {epoch}, {snowflake_generator.epoch}')
+    print(f'process_id: {process_id}, {snowflake_generator.process_id}')
+    print(f'thread_id: {thread_id}, {snowflake_generator.thread_id}')
+    print(f'step: {step}, {snowflake_generator.step}')
+    print(f'sequence: {sequence}, {snowflake_generator.sequence}')
+    print(f'last: {last}, {snowflake_generator.last}')
+    print(f'closed: {snowflake_generator.closed}')
+    
+    for i in range(10):
+        print(f'generated #{i}: {snowflake_generator.generate()}')
+        
+    snowflake_generator.close()
+    
+    print(f'closed: {snowflake_generator.closed}')
